c871b3fdfe1fd6edd68bcfe01f907640a21be53d
Isaac Assegai <isaac.a.travers@gmail.com>
2015-06-01 22:30:18 -0700

    libc: stdio: putchar.c: Add a character to the terminal
    Create a file that will allow us to add a character to the terminal.
    
    Signed-off-by: Isaac Assegai <isaac.a.travers@gmail.com>

---
+++ libc/stdio/putchar.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Isaac Assegai
+ * Date  : 6/1/2015
+ * Put a single character to the terminal.
+ */
+#include <stdio.h>
+
+#if defined(__is_myos_kernel)
+#include <kernel/tty.h>
+#endif
+
+int putchar(int ic){
+#if defined(__is_myos_kernel)
+	char c = (char) ic;
+	terminal_write(&c, sizeof(c));
+#else
+	// TODO: You need to implement a write system call.
+#endif
+	return ic;
+}
