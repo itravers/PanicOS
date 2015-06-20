f28678cee7d41da9ad52b90d7dbae29177b420bf
Isaac Assegai <isaac.a.travers@gmail.com>
2015-06-19 19:26:13 -0700

    porting...

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -36,6 +36,30 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y){
 	terminal_buffer[index] = make_vgaentry(c, color);
 }
 
+void scroll(void){
+    unsigned blank, temp;
+
+    /* A blank is defined as a space... we need to give it
+    *  backcolor too */
+    //blank = 0x20 | (attrib << 8);
+	blank = make_vgaentry(' ', terminal_color);
+
+    /* Row 25 is the end, this means we need to scroll up */
+    if(terminal_row >= 25)
+    {
+        /* Move the current text chunk that makes up the screen
+        *  back in the buffer by a line */
+        temp = terminal_row - 25 + 1;
+        memcpy (VGA_MEMORY, VGA_MEMORY + temp * 80, (25 - temp) * 80 * 2);
+
+        /* Finally, we set the chunk of memory that occupies
+        *  the last line of text to our 'blank' character */
+        memset (VGA_MEMORY + (25 - temp) * 80, blank, 80);
+        terminal_row = 25 - 1;
+    }
+}
+
+
 void terminal_putchar(char c){
 	if(c == '\n'){
 		terminal_column = 0;
@@ -43,12 +67,7 @@ void terminal_putchar(char c){
 	}else{
 		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	}
-	if ( ++terminal_column == VGA_WIDTH ){
-		terminal_column = 0;
-		if ( ++terminal_row == VGA_HEIGHT ){
-			terminal_row = 0;
-		}
-	}
+	scroll(); //scroll if needed
 }
 
 void terminal_write(const char* data, size_t size){
