69e9710e6a0b6c8cc3228783af6136c6b97320d3
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-09-29 14:59:19 -0700

    Added mem command to show where available memory is
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- kernel/arch/i386/mm.c
+++ kernel/arch/i386/mm.c
@@ -17,9 +17,13 @@ int memAmt;
  * Sets up memory location
  */
 void mm_initialize(void){
+  //Get the memory map from the multiboot_info
   multiboot_memory_map_t* mmap = mbt->mmap_addr;
+
+  //loop through the memory map for a usable (1) portion that is bigger than 0x1000000
   while(mmap < mbt->mmap_addr + mbt->mmap_length) {
     if(mmap->type == 1 && mmap->len > 0x1000000){
+      //if found, save that memLoc and memAmt for later use
       memLoc = (int*)mmap->addr;
       memAmt = mmap->len;
     }
