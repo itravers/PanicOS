0a86be4439f863c7fdaa49a4ef31688dee3aa7f5
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-09-28 21:03:33 -0700

    added shell
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

---
+++ kernel/arch/i386/shell.c
@@ -0,0 +1,72 @@
+/**
+ * Author: Isaac Assegai
+ * Date  : 8/27/2017
+ * Controls the initial built in shell
+*/
+
+char lastChar; /* The last character sent from keyboard to shell */
+char* command; /* The command parsed */
+
+void shell_initialize(void){
+  printf("shell initialize \n");
+  lastChar = '\p';
+  
+}
+
+void shell_run(void){
+  printf("Running Shell");
+  while(1){//the shell always runs
+    output_prompt();
+    wait_for_command();
+    process_command(command);
+  }
+}
+
+void output_prompt(){
+  printf("\n");
+  printf("$");
+}
+
+void wait_for_command(){
+  while(lastChar != '\n'){
+    terminal_getRow();//filler, not used except to cause while loop to wait
+  //  printf("\n");
+   // printf(lastChar);
+  }
+  //printf("while loop over");
+  command = "testCommand";
+  command = terminal_getCurrentRowChars();
+}
+
+void process_command(char* command){
+  if(command == 0)return;
+  printf("\n");
+  printf("processing command: %s", command);
+  lastChar = '\P';
+
+  command = 0;
+}
+
+
+
+/**
+ * Receives characters from the keyboard
+ * decides what to do with them,
+ * If there are any special characters it will
+ * handle them.
+ * Send any normal input to the terminal to be output to screen
+ */
+void shell_putchar(char c){
+ //printf(" \n Putting Char:");
+ //terminal_putchar(c);
+  if(c == '\n'){
+    //handle enter key
+    //right now we will just terminal_newline, later we will process a string
+    terminal_newLine();
+  }else{
+    terminal_putchar(c);
+  }
+
+
+    lastChar = c;
+}
---
+++ kernel/include/kernel/shell.h
@@ -0,0 +1,15 @@
+/**
+ * Author: Isaac Assegai
+ * Date  : 9/28/2017
+ * Declares functions that will be used in the built in shell.
+ */
+#ifndef _KERNEL_SHELL_H
+#define _KERNEL_SHELL_H
+
+#include <stddef.h>
+//#include <tty.h>
+
+void shell_initialize(void);
+void shell_run(void);
+
+#endif
