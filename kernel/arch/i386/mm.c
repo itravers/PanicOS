92c02f131c9282a504f5f14c06722ef891f5b1d5
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-09-30 16:44:24 -0700

    Added struct mm_header
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- kernel/arch/i386/mm.c
+++ kernel/arch/i386/mm.c
@@ -5,13 +5,28 @@
 */
 
 #include <kernel/multiboot.h>
+#include <stdbool.h> //for bool type
+#include <stddef.h> //for size_t
 
 /* The multiboot_info passed into kernel main */
 extern struct multiboot_info* mbt;
 
-/* The location of usable memory */
-int* memLoc;
+/* memLoc is a pointer to the beginning of physical memory as determined by multiboot */
+void* memLoc; 
+
+/* memAmt is the amount of bytes in memory as determined by multiboot */
 int memAmt;
+
+/* memEndLoc is a pointer to the end of memory, as calculated in mm_initialze (memLoc+memAmt)*/
+void* memEndLoc;
+
+/* The linkedlist header used for memory allocation. */
+struct mm_header{
+  void* ptr;
+  size_t size;
+  bool used;
+  void* next;
+};
 
 /**
  * Sets up memory location
@@ -29,7 +44,11 @@ void mm_initialize(void){
     }
     mmap = (multiboot_memory_map_t*) ( (unsigned int)mmap + mmap->size + sizeof(mmap->size) );
   }
+
+  /* Calculate a pointer to the end of physical memory. */
+  memEndLoc = memLoc + memAmt;
   printf("\nMemory Found At Location: 0x%x", memLoc);
   printf("\nMemory Amount Located   : 0x%x", memAmt);
+  printf("\nEnd of Phyisical Memeory: 0x%x", memEndLoc);
 
 }
