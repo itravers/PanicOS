30b9f1b8842c1f8a6a2165d52f95ae4e3ec8c263
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-10-01 10:57:46 -0700

    Working on kheap.h
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

---
+++ kernel/include/kernel/kheap.h
@@ -0,0 +1,8 @@
+/**
+ * Author: Isaac Assegai
+ * Date  : 10/1/2017
+ * Interface for the kernels heap functions
+ * Provides a type of malloc (kmalloc) for use before the heap is initialized.
+ */ 
+
+#include <stdlib.h> //for u32int typedefs, etc
--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -1,7 +1,8 @@
 /**
  * Author: Isaac Assegai
  * Date  : 6/1/2015
- * Prototype function used for kernel panics.
+ * Prototype functions 
+ * Command typedefs.
  */
 #ifndef _STDLIB_H
 #define _STDLIB_H 1
@@ -17,6 +18,14 @@ void abort(void);
 
 /** Rounds an unsigned int up to the nearest multiple of a number*/
 unsigned int roundUp(unsigned int, unsigned int);
+
+/** Common typedefs to standardize sizes across platforms*/
+typedef unsigned int u32int;
+typedef int s32int;
+typedef unsigned short u16int;
+typedef short s16int;
+typedef unsigned char u8int;
+typedef char s8int;
 
 #ifdef __cplusplus
 }
