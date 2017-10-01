3084ed9655adf5e1174709c271d896e4abd5c550
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-10-01 12:37:27 -0700

    Adding paging, compiling successful at this point
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- kernel/arch/i386/regs.h
+++ kernel/arch/i386/regs.h
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /* This defines what the stack looks like after an ISR was running */
 struct regs
 {
@@ -6,4 +8,13 @@ struct regs
     unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
     unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */
 };
+
+typedef struct registers
+{
+    u32int ds;                  // Data segment selector
+    u32int edi, esi, ebp, esp, ebx, edx, ecx, eax; // Pushed by pusha.
+    u32int int_no, err_code;    // Interrupt number and error code (if applicable)
+    u32int eip, cs, eflags, useresp, ss; // Pushed by the processor automatically.
+} registers_t;
+
 
--- libc/stdlib/abort.c
+++ libc/stdlib/abort.c
@@ -15,3 +15,35 @@ void abort(void){
 	while ( 1 ) { }
 	__builtin_unreachable();
 }
+
+extern void panic(const char *message, const char *file, u32int line)
+{
+    // We encountered a massive problem and have to stop.
+    asm volatile("cli"); // Disable interrupts.
+
+    printf("PANIC(");
+    printf(message);
+    printf(") at ");
+    printf(file);
+    printf(":");
+    printf("%i", line);
+    printf("\n");
+    // Halt by going into an infinite loop.
+    for(;;);
+}
+
+extern void panic_assert(const char *file, u32int line, const char *desc)
+{
+    // An assertion failed, and we have to panic.
+    asm volatile("cli"); // Disable interrupts.
+
+    printf("ASSERTION-FAILED(");
+    printf(desc);
+    printf(") at ");
+    printf(file);
+    printf(":");
+    printf("%i", line);
+    printf("\n");
+    // Halt by going into an infinite loop.
+    for(;;);
+}
