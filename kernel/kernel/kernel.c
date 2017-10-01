cd8b4839fc0e8586014469f05bfdcbe7882bed44
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-10-01 10:41:56 -0700

    Added formatting and notes to kernel.c
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,35 +1,36 @@
 /* PanicOS - By Isaac Assegai
- * based on bkerndev - Bran's Kernel Development Tutorial
+ * 5/13/2015
+ * Called by start.asm
+ * Responsible for initialzing the Kernel
  */
 
 #include <kernel/multiboot.h>
 
-/*
-typedef struct multiboot_memory_map {
-  unsigned int size;
-  unsigned int base_addr_low,base_addr_high;
-// You can also use: unsigned long long int base_addr; if supported.
-  unsigned int length_low,length_high;
-// You can also use: unsigned long long int length; if supported.
-  unsigned int type;
-} multiboot_memory_map_t;
-*/
+/* Structure passed in by start.asm. Used to access bootloader info
+   We use this in mm.c to find the location of usable memory. */
+struct multiboot_info* mbt;
 
-struct multiboot_info* mbt; /* Need this here so it can be accessed by mm.c */
-
-unsigned char inportb (unsigned short _port)
-{
-    unsigned char rv;
-    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
-    return rv;
+/**
+ * Reads a single byte from an 8-bit port
+ */
+unsigned char inportb (unsigned short _port){
+  unsigned char rv;
+  __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+  return rv;
 }
 
-void outportb (unsigned short _port, unsigned char _data)
-{
-    __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
+/**
+ * Writes a single byte to an 8-bit port
+ */
+void outportb (unsigned short _port, unsigned char _data){
+  __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
 
-//int main(multiboot* multiboot, unsigned int magic){
+/**
+ * Initialze the kernel
+ * Input: mbtt - Info passed into kernel from bootloader
+ *        defined in multiboot.h
+ */
 int main(struct multiboot_info* mbtt, unsigned int magic){
   mbt = mbtt; //we access mbt in mm.c
 //int main(){
@@ -40,53 +41,9 @@ int main(struct multiboot_info* mbtt, unsigned int magic){
   terminal_initialize();
   timer_install();
   keyboard_install();
-  mm_initialize();
-  __asm__ __volatile__ ("sti");
-
-	/*for(int i = 0; i < 20; i++){
-		printf("Hello World!\n");
-	}*/
-//	setWelcomeScreen();
-
-/*
-  int* memory;
-
-  //memory testing
-  printf("\nmbt->flags: 0x%x", mbt->flags);
-  printf("\nmbt->mmap_length: 0x%x", mbt->mmap_length);
-  printf("\nmbt->mem_lower: 0x%x", mbt->mem_lower);
-  printf("\nmbt->mem_upper: 0x%x", mbt->mem_upper);
-  printf("\nmbt->boot_loader_name: 0x%x ", mbt->boot_loader_name);
-  multiboot_memory_map_t* mmap = mbt->mmap_addr;
-  while(mmap < mbt->mmap_addr + mbt->mmap_length) {
-    
-    printf("\nmmap->addr: 0x%x ", mmap->addr);
-    //printf("\nmmap->size: 0x%x", mmap->size);
-    printf("\nmmap->len: 0x%x", mmap->len);
-    printf("\nmmap->type: 0d%i \n", mmap->type);
-
-    if(mmap->type == 1 && mmap->len > 0x1000000){
-      memory = (int*)mmap->addr;
-    }
-
-    mmap = (multiboot_memory_map_t*) ( (unsigned int)mmap + mmap->size + sizeof(mmap->size) );
-    
-  }
-  printf("\nMemory at: 0x%x", memory);
-  printf("\nmbt->mmap_addr: 0x%x ", mbt->mmap_addr);
-  printf("\nmmap->size: 0x%x ", mmap->size);
-  printf("\nmmap->len: 0x%x ", mmap->len);
-*/
-/*
-  short* a = (short*) 0x100000;
-  *a = 0xAAAA ;
-  a++;
-  *a = 0xBBBB;
-  a++;
-  *a = 0xCCCC;
-*/
+  mm_initialize(); 
+  __asm__ __volatile__ ("sti");  //Enable Interrupts
   main_initialize();
-
     for (;;);
 	return 0;
 }
