956cab08d7fdf919f31a58961e1a4b5584d8f625
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-10-01 11:12:06 -0700

    Added kheap.c
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

---
+++ kernel/arch/i386/kheap.c
@@ -0,0 +1,9 @@
+/**
+ * Author: Isaac Assegai
+ * Date  : 10/1/2017
+ * The kernels heap functions
+ * Provides a type of malloc (kmalloc) for use before the heap is initialized.
+ */
+
+#include <kernel/kheap.h>
+
