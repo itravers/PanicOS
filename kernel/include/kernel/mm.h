f5d5dc9a96ce3cb35f6e9912f83a81100c34a690
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-10-02 13:39:35 -0700

    mm.h notes
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- kernel/include/kernel/mm.h
+++ kernel/include/kernel/mm.h
@@ -1,7 +1,7 @@
 /**
  * Author: Isaac Assegai
  * Date  : 9/29/2017
- * Declares functions that will be used to manage memory
+ * Declares public functions that will be used to manage memory
  */
 #ifndef _MM_H
 #define _MM_H
