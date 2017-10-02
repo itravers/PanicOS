09a3ce37e3929b7a867c29192caf4d2a5340718d
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-10-02 11:24:33 -0700

    commenting and formatting paging.c
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- kernel/arch/i386/paging.c
+++ kernel/arch/i386/paging.c
@@ -32,7 +32,7 @@ extern memUsable;
 #define INDEX_FROM_BIT(a) (a/(8*4))
 #define OFFSET_FROM_BIT(a) (a%(8*4))
 
-/* Marks a frame as "set" in the frames[] bitset */
+/* Marks a frame as used in the frames[] bitset. */
 static void set_frame(u32int frame_addr){
   u32int frame = frame_addr/0x1000;
   u32int idx = INDEX_FROM_BIT(frame);
@@ -40,80 +40,65 @@ static void set_frame(u32int frame_addr){
   frames[idx] |= (0x1 << off);
 }
 
-// Static function to clear a bit in the frames bitset
-static void clear_frame(u32int frame_addr)
-{
-    u32int frame = frame_addr/0x1000;
-    u32int idx = INDEX_FROM_BIT(frame);
-    u32int off = OFFSET_FROM_BIT(frame);
-    frames[idx] &= ~(0x1 << off);
+/* Marks a frame as unused in the frames[] bitset. */
+static void clear_frame(u32int frame_addr){
+  u32int frame = frame_addr/0x1000;
+  u32int idx = INDEX_FROM_BIT(frame);
+  u32int off = OFFSET_FROM_BIT(frame);
+  frames[idx] &= ~(0x1 << off);
 }
 
-// Static function to test if a bit is set.
-static u32int test_frame(u32int frame_addr)
-{
-    u32int frame = frame_addr/0x1000;
-    u32int idx = INDEX_FROM_BIT(frame);
-    u32int off = OFFSET_FROM_BIT(frame);
-    return (frames[idx] & (0x1 << off));
+/* Tests if a Frame at frame_address is set in the frames[] bitset. */
+static u32int test_frame(u32int frame_addr){
+  u32int frame = frame_addr/0x1000;
+  u32int idx = INDEX_FROM_BIT(frame);
+  u32int off = OFFSET_FROM_BIT(frame);
+  return (frames[idx] & (0x1 << off));
 }
 
-// Static function to find the first free frame.
-static u32int first_frame()
-{
-    u32int i, j;
-    for (i = 0; i < INDEX_FROM_BIT(nframes); i++)
-    {
-        if (frames[i] != 0xFFFFFFFF) // nothing free, exit early.
-        {
-            // at least one bit is free here.
-            for (j = 0; j < 32; j++)
-            {
-                u32int toTest = 0x1 << j;
-                if ( !(frames[i]&toTest) )
-                {
-                    return i*4*8+j;
-                }
-            }
+/* Returns index of the first unused frame in the frame[] bitset. */
+static u32int first_frame(){
+  u32int i, j;
+  for (i = 0; i < INDEX_FROM_BIT(nframes); i++){
+    if (frames[i] != 0xFFFFFFFF){ // nothing free, exit early.
+      // at least one bit is free here.
+      for (j = 0; j < 32; j++){
+        u32int toTest = 0x1 << j;
+        if (!(frames[i]&toTest)){
+          return i*4*8+j;
         }
+      }
     }
+  }
 }
 
-// Function to allocate a frame.
-void alloc_frame(page_t *page, int is_kernel, int is_writeable)
-{
-    if (page->frame != 0)
-    {
-        return;
+/* Allocate a new frame */
+void alloc_frame(page_t *page, int is_kernel, int is_writeable){
+  if (page->frame != 0){
+    return;
+  }else{
+    u32int idx = first_frame();
+    if (idx == (u32int)-1){
+      // PANIC! no free frames!!
+      PANIC("NO Free Frames!");
     }
-    else
-    {
-        u32int idx = first_frame();
-        if (idx == (u32int)-1)
-        {
-            // PANIC! no free frames!!
-        }
-        set_frame(idx*0x1000);
-        page->present = 1;
-        page->rw = (is_writeable)?1:0;
-        page->user = (is_kernel)?0:1;
-        page->frame = idx;
-    }
+    set_frame(idx*0x1000);
+    page->present = 1;
+    page->rw = (is_writeable)?1:0;
+    page->user = (is_kernel)?0:1;
+    page->frame = idx;
+  }
 }
 
-// Function to deallocate a frame.
-void free_frame(page_t *page)
-{
-    u32int frame;
-    if (!(frame=page->frame))
-    {
-        return;
-    }
-    else
-    {
-        clear_frame(frame);
-        page->frame = 0x0;
-    }
+// Deallocated a previously allocated frame. */
+void free_frame(page_t *page){
+  u32int frame;
+  if (!(frame=page->frame)){
+    return;
+  }else{
+    clear_frame(frame);
+    page->frame = 0x0;
+  }
 }
 
 void initialise_paging()
